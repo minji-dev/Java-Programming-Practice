adebad92dfc2b01ab4ac1f272326fec1c405e00e
Kohminchae <mc0jooo@gmail.com>
2022-05-24 01:04:26 +0900

    resolve merge conflict

--- final/src/main.java
+++ final/src/main.java
@@ -16,13 +16,7 @@ public class main {
 			int clubCnt = Integer.parseInt(brC.readLine());
 			clubs = new club[clubCnt];
 			for(int i=0; i<clubCnt; i++) {
-				String[] tmp = brC.readLine().split(", ");
-				int c = Integer.parseInt(tmp[2]);
-				clubs[i] = new club(tmp[0], tmp[1], c);
-				for(int j=0; j<c; j++) {
-					String[] tmp2 = brC.readLine().split(", ");
-					clubs[i].setReview(tmp2[0], tmp2[1], c);
-				}
+
 			}
 			
 			int userCnt = Integer.parseInt(brU.readLine());
@@ -32,8 +26,5 @@ public class main {
 				users[i] = new user(tmp[0], tmp[1], tmp[2]);
 			}
 		} catch (IOException e) { e.printStackTrace(); }
-		
-		
 	}
-
 }
