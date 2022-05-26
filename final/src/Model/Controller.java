84b1a69f02519b45197a9dd993a8c929f39677f4
Kohminchae <mc0jooo@gmail.com>
2022-05-26 23:00:34 +0900

    push before conflict

--- final/src/Model/Controller.java
+++ final/src/Model/Controller.java
@@ -1,6 +1,8 @@
 package Model;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -9,11 +11,11 @@ import java.io.IOException;
 
 import javax.swing.*;
 
-public class Controller {
+public class Controller extends JFrame implements ActionListener {
 
 	public static club clubs[]; public static int clubCnt;
 	public static user users[];	public static int userCnt;
-
+	public static JLabel ClubDetail = new JLabel();
 	public static void init() throws IOException {
 		BufferedReader brC = new BufferedReader(new FileReader("./club.txt"));
 		BufferedReader brU = new BufferedReader(new FileReader("./user.txt"));
@@ -39,29 +41,6 @@ public class Controller {
 			}
 			brC.close(); brU.close();
 
-			//gui
-			JFrame f = new JFrame("total");
-			JFrame f1 = new JFrame("CLUB");
-			JFrame f2 = new JFrame("CLUB INTRO");
-			GridLayout g1 = new GridLayout(1,2);
-			GridLayout g2 = new GridLayout(clubCnt%3+1,3);
-			
-			//f1.setLayout(g1);
-			f2.setLayout(g2);
-
-			f.add(f1); f.add(f2);
-
-			JLabel[] l1 = new JLabel[2]; 
-			
-			l1[0].setText("Title");
-			l1[1].setText("login");
-
-			JLabel[] l2 = new JLabel[clubCnt];
-			for(int i=0;i<clubCnt;i++) {
-				l1 = new 
-				l2[i].setText(clubs[i].name);
-				l2[i].setText(clubs[i].intro);
-			}
 		} catch (IOException e) { e.printStackTrace(); }
 	}
 	
@@ -75,30 +54,55 @@ public class Controller {
 			System.out.println(users[i].id + " in " + users[i].club);
 	
 		//gui
-		int a = 2;
-		JFrame f1 = new JFrame("CLUB");
-		//JFrame f2 = new JFrame("CLUB INTRO");
-		GridLayout g1 = new GridLayout(a,1);
-		//GridLayout g2 = new GridLayout(clubCnt,3);
-		f1.setLayout(g1);
-		//f2.setLayout(g2);
-		
-		JLabel[] l1 = new JLabel[2];
-		l1[0] = new JLabel();
+		JFrame f = new JFrame("CLUB");
+		JLabel L1 = new JLabel();
+		JLabel L2 = new JLabel();
+		GridLayout g1 = new GridLayout(1,2);
+		GridLayout g2 = new GridLayout(clubCnt/3+1,3);
+		System.out.print(clubCnt);
+		L1.setLayout(g1);
+		L2.setLayout(g2);
+
+		JLabel[] l1 = new JLabel[2]; 
+		l1[0] = new JLabel(); l1[1] = new JLabel();
 		l1[0].setText("Title");
-		l1[1] = new JLabel();
 		l1[1].setText("login");
-		f1.add(l1[0]);
-		f1.add(l1[1]);
-		f1.setSize(700, 300);
-        f1.setVisible(true);
-		//JLabel[] l2 = new JLabel[clubCnt];
-		for(int i=0;i<2;i++) {
-			l1[i].setText(clubs[i].name);
-			l1[i].setText(clubs[i].intro);
+		
+		L1.add(l1[0]); L1.add(l1[1]);
+		
+		ActionListener listen = new ActionListener() {
+			@Override
+            public void actionPerformed(ActionEvent e) {
+				showDetail(e.getActionCommand());
+			}
+		};
+		
+		JButton[] b1 = new JButton[clubCnt];
+		for(int i=0;i<clubCnt;i++) {
+			b1[i] = new JButton(clubs[i].name);
+			b1[i].addActionListener(listen);
+			L2.add(b1[i]);
 		}
+		L1.setSize(700, 50);
+		L1.setVisible(true);
+		L2.setSize(700, 300);
+		L2.setVisible(true);
+		
+		f.add(L1); 
+		f.add(L2);
+		f.setVisible(true);
+		f.setSize(700,350);
 	}
-	
+	public static void showDetail(String name) {
+		JLabel des = new JLabel();
+		JLabel 
+		for(int i=0;i<clubsCnt;i++) { 
+			if
+		} // 동아리 찾기
+		des.setText = 입력
+		ClubDetail.setSize(300,300);
+		ClubDetail.setVisible(true);
+	}
 	public static void save() throws IOException {
 		BufferedWriter bwC = new BufferedWriter(new FileWriter("./club.txt"));
 		BufferedWriter bwU = new BufferedWriter(new FileWriter("./user.txt"));
@@ -116,7 +120,8 @@ public class Controller {
 			bwC.flush(); bwU.flush(); bwC.close(); bwU.close(); 
 		} catch (IOException e) { e.printStackTrace(); }
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		init();
 		print();
 	}
 }
