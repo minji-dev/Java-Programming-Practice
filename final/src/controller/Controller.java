43304005acf54be4b4b51f0cee2288aa7147052d
Kohminchae <mc0jooo@gmail.com>
2022-05-27 15:17:51 +0900

    add buttons (add Club, Login)

--- final/src/controller/Controller.java
+++ final/src/controller/Controller.java
@@ -20,11 +20,11 @@ public class Controller extends JFrame {
 	public static ArrayList<user> users = new ArrayList<user>();
 	public static JFrame f = new JFrame("CLUB");
 	public static JLabel[] ClubDetail;
-	
+
 	public static void init() throws IOException {
 		BufferedReader brC = new BufferedReader(new FileReader("./club.txt"));
 		BufferedReader brU = new BufferedReader(new FileReader("./user.txt"));
-		
+
 		try {
 			int clubCnt = Integer.parseInt(brC.readLine());
 			for(int i=0; i<clubCnt; i++) {
@@ -47,20 +47,38 @@ public class Controller extends JFrame {
 			brC.close(); brU.close();
 		} catch (IOException e) { e.printStackTrace(); }
 	}
+
+public static void print() {
 	
-	public static void print() {
-		
 		GridBagLayout gb3 = new GridBagLayout();
-		gb3.columnWidths = new int[] {100,100,100,300};
 		f.setLayout(gb3);
 		GridBagConstraints g_11 = new GridBagConstraints(); g_11.fill = GridBagConstraints.BOTH;
 		
 		GridBagConstraints gl = new GridBagConstraints(); gl.fill = GridBagConstraints.BOTH;
-		JLabel l11 = new JLabel(); l11.setText("Title"); 
-		JLabel l12 = new JLabel(); l12.setText("login");
-		g_11.gridx = 0; g_11.gridy = 0; g_11.gridwidth = 3;
+		JLabel l11 = new JLabel(); l11.setText("SKKU CLUB"); 
+		JTextField input_id = new JTextField(8);
+		JTextField input_pw = new JTextField(8);
+		
+		ActionListener listenLog = new ActionListener() {
+			@Override
+		    public void actionPerformed(ActionEvent e) {
+				// call Login
+				// String getId = input_id.getText();
+				// String getPw = input_pw.getText();
+			}
+		};	
+		JButton l12 = new JButton("login"); l12.addActionListener(listenLog);
+		
+		g_11.gridx = 0; g_11.gridy = 0;
 		f.add(l11,g_11); 
-		g_11.gridx = 4; g_11.gridy = 0; g_11.gridwidth = 3;
+		
+		g_11.gridx = 3; g_11.gridy = 0;
+		f.add(input_id,g_11);
+		
+		g_11.gridx = 4; g_11.gridy = 0;
+		f.add(input_pw,g_11);
+		
+		g_11.gridx = 5; g_11.gridy = 0;
 		f.add(l12,g_11);
 		
 		ActionListener listen = new ActionListener() {
@@ -71,29 +89,47 @@ public class Controller extends JFrame {
 		};
 
 		JButton[] b1 = new JButton[clubs.size()];
+		GridBagConstraints g1 = new GridBagConstraints(); g1.fill = GridBagConstraints.BOTH;
+
 		for(int i=0;i<clubs.size();i++) {
-			GridBagConstraints g1 = new GridBagConstraints(); g1.fill = GridBagConstraints.BOTH;
 			b1[i] = new JButton(clubs.get(i).name);
 			b1[i].addActionListener(listen);
-			g1.gridx = 1; g1.gridy = i+1;
+			g1.gridx = i; g1.gridy = 1;
 			f.add(b1[i],g1);
+		}
+		// 동아리 등록 
+		ActionListener AddClub = new ActionListener() {
+			@Override
+		    public void actionPerformed(ActionEvent e) {
+				// 등록 칸으로 가기
+			}
+		};
+		
+		JButton plus = new JButton("동아리 등록"); plus.addActionListener(AddClub);
+		g1.gridx = clubs.size(); g1.gridy = 1;
+		f.add(plus,g1);
+
+		for(int i=clubs.size()+1;i<12;i++) {
+			JButton nonb = new JButton(" ");
+			g1.gridx = i%6; g1.gridy = (int)(i/6)+1;
+			f.add(nonb,g1);
 		}
 
 		f.setSize(1000,1000);
 		f.setVisible(true);
 	}
-	
+
 	public static void showDetail(String name) {
 		int i;
 
 		GridBagLayout gb = new GridBagLayout();
 		JFrame f1 = new JFrame(name);
 		f1.setLayout(gb);
-		
+
 		for(i=0;i<clubs.size() && clubs.get(i).name != name;i++) { }
 		club c = clubs.get(i);
-		
-		GridBagLayout gbin = new GridBagLayout();
+
+		//GridBagLayout gbin = new GridBagLayout();
 		JLabel intro = new JLabel(); intro.setText(name+" 소개\n: "+c.intro);
 		GridBagConstraints g = new GridBagConstraints(); g.fill = GridBagConstraints.BOTH;
 		g.gridx = 0; g.gridy = 2; g.gridwidth = 6;
@@ -103,11 +139,11 @@ public class Controller extends JFrame {
 			JLabel label = new JLabel(); label.setText(c.list.get(j).id);
 			g.gridx = 0; g.gridy = 3+j; g.gridwidth = 2;
 			f1.add(label,g);
-			
+
 			JLabel label1 = new JLabel(); label1.setText(c.list.get(j).text);
 			g.gridx = 2; g.gridy = 3+j; g.gridwidth = 3;
 			f1.add(label1,g);
-			
+
 			JButton b = new JButton("쪽지");
 			g.gridx = 5; g.gridy = 3+j;
 			f1.add(b,g);
@@ -132,7 +168,7 @@ public class Controller extends JFrame {
 	public static void save() throws IOException {
 		BufferedWriter bwC = new BufferedWriter(new FileWriter("./club.txt"));
 		BufferedWriter bwU = new BufferedWriter(new FileWriter("./user.txt"));
-		
+
 		try {
 			bwC.write(clubs.size()+"\n");
 			for(int i=0; i<clubs.size(); i++) {
@@ -146,7 +182,7 @@ public class Controller extends JFrame {
 			bwC.flush(); bwU.flush(); bwC.close(); bwU.close(); 
 		} catch (IOException e) { e.printStackTrace(); }
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		init();
 		print();
